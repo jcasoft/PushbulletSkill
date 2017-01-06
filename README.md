1dea4d3718037ba6a786305aa298b680452897a1
jcasoft <jca@jcasoft.com>
2017-01-06 17:37:19 -0600

    Skill updated for use with new API

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 **PushbulletSkill**
 ===================
 
-
-An skill to use with Mycroft that allows you to send messages and photos using **Pushbullet**
+For Mycroft with new API (https://home.mycroft.ai) 
+An skill to use with Mycroft that allows you to send messages, 10 seconds audio file and photos using **Pushbullet**
+New features: message to self account and help audio file to devices
 
 This uses  [pushbullet.py](https://pypi.python.org/packages/7d/a8/7fbed382824e84a51dfdc13315d9171fb6dc0670803ccb400931b9e3465b/pushbullet.py-0.10.0.tar.gz#md5=24db6917a12e1c9b3fecca102615376b)
 
@@ -19,17 +20,23 @@ Download pushbullet.py-0.10.0.tar.gz from link listed  **above**
 
 > **Note:**
 
-> - You have to install pushbullet inside Mycroft virtual environment, for that do the following, download pushbullet
+> - Install audio convert tool
+
+     sudo apt-get install lame
+
+
+> - Download pushbullet
 
      tar -xvzf pushbullet.py-0.10.0.tar.gz
      cd pushbullet.py-0.10.0
-     sudo /home/pi/.virtualenvs/mycroft/local/bin/python setup install
+
+> - Now install pushbullet inside Mycroft virtual environment, for that do the following
+     workon mycroft
+     python setup.py install
 
 - Create your account on  [pushbullet.com](https://www.pushbullet.com/) to get your [Access Token](https://www.pushbullet.com/#settings)
 - Install pushbullet on your phone 
 - Make some contacts
-
-
 
 
 
@@ -40,26 +47,19 @@ Installation
 -------------------
 **Clone PushbulletSkill repository** on third party skill directory
 
-    cd  /opt/mycroft/third_party/
+    cd  $HOME/.mycroft/skills
 
     git clone  https://github.com/jcasoft/PushbulletSkill.git
 
 <i class="icon-cog"></i>Add 'PushbulletSkill' section in your Mycroft configuration file on: 
 
-/home/pi/.mycroft/mycroft.ini
+    $HOME/.mycroft/mycroft.conf
 
-    [PushbulletSkill]
-    api_key = "o.XXXXXXXXXX"  # <-- Replace with your Access token
-    
-    photo = "/opt/mycroft/third_party/mycroft-pushbullet-skill/script/photo.py"
-    
-    photo_img = "/tmp/photo.png"
+	"PushbulletSkill": {
+		"api_key": "o.XXXXXXXXXX"  # <-- Replace with your Access token,
+	}
 
-> **Note:**
 
-> - If use a different **third party skill directory** you have to change on [PushbulletSkill] section on /home/pi/.mycroft/mycroft.ini 
-> and line 39 on 
-> `/opt/mycroft/third_party/mycroft-pushbullet-skill/script/photo.py`
 
 Restart Skills
 
@@ -74,20 +74,27 @@ Features
 
 Currently this skill can do the following things (with some variation):
 
-- **push** *juancarlos* barcelona is the best soccer team of the world **stop**
-  
+- push juancarlos barcelona is the best soccer team of the world
+- photo
+- push me remember to see the walking dead
+- help "i had an accident and i need help" in audio file (not text)
 
- > **Note:**
-> - **push** is a PushMessageKeyword
-> - ***juancarlos***  is the name of a contact that must exist in your pushbullet account, you can to pronounce the name or a part of the name from the beginning, if there is no contact, mycroft say a message that the contact not exist.
-> -  **barcelona is the best soccer team of the world** is the message
-> - **stop** to finish the message
+> **Note:**
+> - push - to invoke a push message to a contact
+> - juancarlos - is the name of a contact that must exist in your pushbullet account, you can to pronounce the name or a part of the name from the beginning, if there is no contact, mycroft say a message that the contact not exist.
+> - barcelona is the best soccer team of the world - is the message
 
-- **photo**
- > **Note:**
-> - **photo** is a PushMessageKeyword
+> **Note:**
+> - photo - to invoke a push photo to your own pushbullet account
 > - The photo is sent to your pushbullet account
 
+> **Note:**
+> - push me - to invoke a push message to your own pushbullet account
+> - The photo is sent to your pushbullet account
+
+> **Note:**
+> - help - to invoke a push 10 seconds audio file to your own pushbullet account
+> - The audio file is sent to your devices associated to you pushbullet account
 
 
 **Enjoy !**
--- __init__.py
+++ __init__.py
@@ -1,24 +1,6 @@
-# Copyright 2016 Mycroft AI, Inc.
-#
-# This file is part of Mycroft Core.
-#
-# Mycroft Core is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Mycroft Core is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Mycroft Core.  If not, see <http://www.gnu.org/licenses/>.
-
-
 
 import time
-from os.path import dirname, join
+from os.path import dirname, join, expanduser
 import os
 
 
@@ -38,7 +20,7 @@ __author__ = 'jcasoft'
 
 
 LOGGER = getLogger(__name__)
-
+ 
 
 class PushbulletSkill(MycroftSkill):
     """
@@ -52,8 +34,11 @@ class PushbulletSkill(MycroftSkill):
         self.api_key = self.config.get('api_key')
 	self.contact = ''
 	self.chat = ''
-	self.photo_script = self.config.get('photo')
-	self.photo_img = self.config.get('photo_img')
+
+	self.photo_script = expanduser("~")+"/.mycroft/skills/PushbulletSkill/script/photo.py"
+	self.photo_img = "/tmp/photo.png"
+	self.help_audio = "/tmp/help"
+
 
 	"""
 	Register Mycroft device to Pushbullet
@@ -63,7 +48,8 @@ class PushbulletSkill(MycroftSkill):
 	Get the contacts list from Pushbullet Mycroft device registered
 	"""
 	self.contactspb = self.pb.chats
-	
+	self.devicespb = self.pb.devices
+
 	self.contacts={}
 	for i in range(len(self.contactspb)):	
 	    row_contact = str(self.contactspb[i])
@@ -72,48 +58,99 @@ class PushbulletSkill(MycroftSkill):
 	    email = row_contact.split("<",1)[1].split(">")[0]
 	    self.contacts[name] = self.contactspb[i]
 
+	self.devices={}
+	for i in range(len(self.devicespb)):	
+	    row_device = str(self.devicespb[i])
+	    name = row_device.split("'",1)[1].split("'")[0]
+	    name = name.replace(" ","").lower()
+	    self.devices[name] = self.devicespb[i]
 
     def initialize(self):
         self.load_data_files(dirname(__file__))
         self.load_regex_files(join(dirname(__file__), 'regex', self.lang))
 
+        intent = IntentBuilder("PushMessageIntent")\
+        	.require("PushMessageKeyword")\
+		.require("Contact")\
+		.require("Message")\
+		.build()
+        self.register_intent(intent, self.handle_pushmessage)
 
-        pushmessage_intent = IntentBuilder("PushMessageIntent").\
-            require("PushMessageKeyword").require("Contact").require("Message").build()
-        self.register_intent(pushmessage_intent, self.handle_pushmessage)
+        intent = IntentBuilder("PushMeMessageIntent")\
+        	.require("PushMessageKeyword")\
+		.require("MeKeyword")\
+		.require("SelfMessage")\
+		.build()
+        self.register_intent(intent, self.handle_push_me_message)
 
-        pushphoto_intent = IntentBuilder("PushPhotoSkillIntent").require(
-            "PushPhotoStartKeyword").build()
-        self.register_intent(pushphoto_intent, self.handle_pushphoto)
+        intent = IntentBuilder("PushPhotoIntent")\
+		.require("PushPhotoKeyword")\
+		.build()
+        self.register_intent(intent, self.handle_pushphoto)
+
+        intent = IntentBuilder("PushHelpIntent")\
+		.require("PushHelpKeyword")\
+		.build()
+        self.register_intent(intent, self.handle_help)
 
     def handle_pushmessage(self, message):
         try:
-            contact = message.metadata.get("Contact").lower()
+            contact = message.data.get("Contact").lower()
             if contact in self.contacts:
 		"""
     		Evaluate a exact contact to send message using Pushbullet
     		"""
                 chat = self.contacts.get(contact)
-                msg = message.metadata.get("Message")
+                msg = message.data.get("Message")
 	   	push = self.pb.push_note("Message from Mycroft",msg, chat=chat)
 	   	data = {"contact": contact}
-	   	self.speak_dialog("pushmessage",data)
+	   	self.speak_dialog("push.message",data)
+            elif contact in self.devices:
+		"""
+    		Evaluate a exact device to send message using Pushbullet
+    		"""
+                device = self.devices.get(contact)
+                msg = message.data.get("Message")
+	   	push = self.pb.push_note("Message from Mycroft",msg, device=device)
+	   	data = {"contact": contact}
+	   	self.speak_dialog("push.device",data)
 	    else:
 		"""
     		Evaluate a similar contact to send message using Pushbullet
     		"""
-		matching = [s for s in self.contacts if contact in s]
-		matching = str(matching)
-		name_match = matching.split("'",1)[1].split("'")[0]
-		chat = self.contacts.get(name_match)
-                msg = message.metadata.get("Message")
-	   	push = self.pb.push_note("Message from Mycroft",msg, chat=chat)
-	   	data = {"contact": name_match}
-	   	self.speak_dialog("pushmessage",data)
+		matching_contact = [s for s in self.contacts if contact in s]
+		matching_device = [d for d in self.devices if contact in d]
+
+		matching_contact = str(matching_contact)
+		matching_device = str(matching_device)
+
+		if len(matching_contact) > 2:
+			name_match = matching_contact.split("'",1)[1].split("'")[0]
+			chat = self.contacts.get(name_match)
+	                msg = message.data.get("Message")
+		   	push = self.pb.push_note("Message from Mycroft",msg, chat=chat)
+		   	data = {"contact": name_match}
+		   	self.speak_dialog("push.message",data)
+		elif len(matching_device) > 2:
+			name_match = matching_device.split("'",1)[1].split("'")[0]
+			device = self.devices.get(name_match)
+	                msg = message.data.get("Message")
+			full_name = str(device).split("'")
+			full_name = full_name[1]
+		   	push = self.pb.push_note("Message from Mycroft",msg, device=device)
+		   	data = {"contact": full_name}
+		   	self.speak_dialog("push.device",data)
 
         except Exception as e:
 	    data = {"contact": contact}
 	    self.speak_dialog("processing.pushmessage",data)
+
+
+    def handle_push_me_message(self, message):
+
+	msg = message.data.get("SelfMessage")
+   	push = self.pb.push_note("Message from Mycroft",msg)
+   	self.speak_dialog("push.self.message")
 
 
     def	handle_pushphoto(self, message):
@@ -128,6 +165,22 @@ class PushbulletSkill(MycroftSkill):
 	    file_data = self.pb.upload_file(png, "Photo from Mycroft")
         chat = self.chat
   	push = self.pb.push_file(**file_data)
+
+    def	handle_help(self, message):
+	self.speak_dialog("push.help")
+	time.sleep(6)
+	play_wav("ding.wav")
+	audio_cmd = "arecord -r 16000 -c 2 -d 10 -D plughw:0 " + self.help_audio+".wav"
+	os.system(audio_cmd)
+	play_wav("dong.wav")
+	audio_cmd = "lame " + self.help_audio+".wav " + self.help_audio+".mp3"
+	os.system(audio_cmd)
+	with open(self.help_audio+".mp3","rb") as mp3:
+	    file_data = self.pb.upload_file(mp3, "***Help: Audio Message from Mycroft***")
+	chat = self.pb.devices
+  	push = self.pb.push_file(**file_data)
+	self.speak_dialog("push.help.send")
+
 	
     def stop(self):
         pass
